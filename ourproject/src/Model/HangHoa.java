6fda68c03d93cce4b7f9b35c4231c663e2c7eabe
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2025-08-22 06:58:03 +0000

    khoi tao ung dung

---
+++ ourproject/src/Model/HangHoa.java
@@ -0,0 +1,17 @@
+public class HangHoa {
+
+    //biến thành viên
+
+    public String maHangHoa;
+    public String tenHangHoa;
+    public String nhaSanXuat;
+    public String nuocSanXuat;
+
+    //phuong thuc hoat dong
+    
+    String layTenHangHoa(String maHangHoa){
+        return tenHangHoa;
+    }
+    
+    
+}
---
+++ ourproject/src/Test/testHangHoa.java
@@ -0,0 +1,16 @@
+public class testHangHoa {
+
+
+    public static void test(){
+
+        HangHoa testHH = new HangHoa();
+
+        testHH.tenHangHoa ="Dienthoai iphone";
+        testHH.maHangHoa = "ma01";
+        //in ten hang hoa voi ma tuong ung
+        System.out.println(testHH.layTenHangHoa("ma01"));
+        
+
+    }
+    
+}
