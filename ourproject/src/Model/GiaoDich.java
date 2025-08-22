7de247decc337ca0f3328f4e22c56e24ee242ef2
Cjayze <Douglasm2210@gmail.com>
2025-08-22 15:16:14 +0700

    update

--- ourproject/src/Model/GiaoDich.java
+++ ourproject/src/Model/GiaoDich.java
@@ -1,3 +1,9 @@
 public class GiaoDich {
-    
+        public String sanpham;
+        public String soLuong;
+        
+        //phuong thuc hoat dong
+        String layThongTinGiaoDich() {
+                return "Sản phẩm: " + sanpham + ", Số lượng: " + soLuong;
+        }
 }
