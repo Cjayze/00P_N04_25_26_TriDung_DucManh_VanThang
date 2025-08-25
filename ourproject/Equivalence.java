aa33b38366976f823590885c5862a72c78bac536
Cjayze <Douglasm2210@gmail.com>
2025-08-25 18:16:08 +0700

    khoi tao

---
+++ ourproject/Equivalence.java
@@ -0,0 +1,8 @@
+public class Equivalence {
+    public static void main(String[] args) {
+        Integer n1 = 47;
+        Integer n2 = 47;
+        System.out.println(n1.equals(n2)); // true
+        System.out.println(n1 != 2); // true
+    }
+}
