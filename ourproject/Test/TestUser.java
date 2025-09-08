e6b259789752ef678c8c0076e7dead234423fbf7
Cjayze <Douglasm2210@gmail.com>
2025-09-08 17:52:31 +0700

    update

---
+++ ourproject/Test/TestUser.java
@@ -0,0 +1,10 @@
+package Test;
+import src.User;
+
+public class TestUser {
+    public static void run() {
+        User u = new User("Manh", "manh@example.com");
+        System.out.println("Ten: " + u.getName());
+        System.out.println("Email: " + u.getEmail());
+    }
+}
---
+++ ourproject/src/App.java
@@ -0,0 +1,14 @@
+package src;
+
+import Test.TestUser;
+import Test.TestTime;
+import Test.TestRecursion;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("=== Run Practice 3 ===");
+        TestUser.run();
+        TestTime.run();
+        TestRecursion.run();
+    }
+}
