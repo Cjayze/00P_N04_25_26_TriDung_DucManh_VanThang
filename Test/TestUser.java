bf06fc18e705073c2ace64d63383c096aaa48593
Thang-2005 <nguyenthang2611205@gmail.com>
2025-09-15 11:14:01 +0000

    defactor code

--- Test/TestUser.java
+++
@@ -1,8 +0,0 @@
-public class TestUser {
-    public static void run() {
-        User user = new User("manh", "manh@example.com", "123456");
-        user.displayInfo();
-        user.setEmail("newmail@example.com");
-        user.displayInfo();
-    }
-}
